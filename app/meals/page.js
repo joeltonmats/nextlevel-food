9d8979e057fd8034c49d5eaf0a6ec9c1e07834b4
Joelton Matos <joeltonmatos@gmail.com>
2025-03-19 09:35:18 -0300

    feat(meals): enhance meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,14 @@
 import { Suspense } from "react";
 import Link from "next/link";
 
-import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meals shared by our vibrant community.",
+};
 
 async function Meals() {
   const meals = await getMeals();
